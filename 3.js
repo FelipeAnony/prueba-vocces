cc6c3ee2612475954ef594b0b7129dbf73e905e6
Felipe Mendonca <felipemom2001@gmail.com>
2022-09-12 16:30:38 +0200

    feat: complete prueba 3

--- 3.js
+++ 3.js
@@ -4,10 +4,24 @@
 // Ejemplo: en el caso de [1, 5, 3, 5] existen 2 incremetos ( el día 2 respecto al dia 1 y el dia 4 respecto al día 3 )
 
 function increasedMeasurements(measurements) {
+  if (!Array.isArray(measurements)) return null;
+
+  let increasedDaysCount = 0;
+
+  measurements.forEach((e, i) => {
+    if (i > 0 && measurements[i - 1] < e) increasedDaysCount++;
+  });
+
+  return increasedDaysCount;
 }
 
 /**
  * TEST Ejercicio 1
  */
-const measurements = [245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222, 221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234]
-increasedMeasurements(measurements) // 19
+const measurements = [
+  245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222,
+  221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234,
+];
+increasedMeasurements(measurements); // 19
+
+module.exports.increasedMeasurements = increasedMeasurements;
---
+++ tests/3.test.js
@@ -0,0 +1,18 @@
+const { increasedMeasurements } = require('../3');
+
+describe('Prueba 1', () => {
+  it('Should return null if the param provided is not an array', () => {
+    const notAnArray = {};
+
+    expect(increasedMeasurements(notAnArray)).toBeNull();
+  });
+
+  it('Should return the amount of days that the measurement increased', () => {
+    const array1 = [
+      245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222,
+      221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234,
+    ];
+
+    expect(increasedMeasurements(array1)).toEqual(19);
+  });
+});
