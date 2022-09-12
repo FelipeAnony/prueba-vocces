e0950aca4d450ebfa5dd0d2e6aa7915cbb4c56b7
Felipe Mendonca <felipemom2001@gmail.com>
2022-09-12 14:54:54 +0200

    feat: complete prueba 1

--- 1.js
+++ 1.js
@@ -3,11 +3,47 @@
 // crea una función findUniq que retorne el elemento único.
 
 function findUniq(array) {
-}
+  if (!Array.isArray(array)) return null;
 
+  const itemsCount = {};
+
+  array.forEach((e) => {
+    if (typeof e === 'string') e = String(e);
+
+    if (itemsCount[e]) {
+      itemsCount[e]++;
+    } else {
+      itemsCount[e] = 1;
+    }
+  });
+
+  for (let element in itemsCount) {
+    if (itemsCount[element] === 1) {
+      // la funcion podria acabar con un 'return element'.
+      // Esto abajo es para garantizar que devuelva el elemento original,
+      // de modo que su tipo de dato no sea alterado de 1 para '1' por ejemplo.
+
+      const originalArrayItem = array.find((e) => e == element);
+      return originalArrayItem;
+    }
+  }
+}
 
 /**
  * TEST Ejercicio 1
  */
-findUniq(['12', 10, '12', 11, 1, 11, 10, '12']) // 1
-findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']) // 'Wonder Woman'
+findUniq(['12', 10, '12', 11, 1, 11, 10, '12']); // 1
+
+findUniq([
+  'Capitán América',
+  'Hulk',
+  'Deadpool',
+  'Capitán América',
+  'Hulk',
+  'Wonder Woman',
+  'Deadpool',
+  'Iron Man',
+  'Iron Man',
+]); // 'Wonder Woman'
+
+module.exports.findUniq = findUniq;
---
+++ tests/1.test.js
@@ -0,0 +1,27 @@
+const { findUniq } = require('../1');
+
+describe('Prueba 1', () => {
+  it('Should return null if the param provided is not an array', () => {
+    const notAnArray = {};
+
+    expect(findUniq(notAnArray)).toBeNull();
+  });
+
+  it('Should return the unique element of the list', () => {
+    const array1 = ['12', 10, '12', 11, 1, 11, 10, '12'];
+    const array2 = [
+      'Capitán América',
+      'Hulk',
+      'Deadpool',
+      'Capitán América',
+      'Hulk',
+      'Wonder Woman',
+      'Deadpool',
+      'Iron Man',
+      'Iron Man',
+    ];
+
+    expect(findUniq(array1)).toBe(1);
+    expect(findUniq(array2)).toBe('Wonder Woman');
+  });
+});
