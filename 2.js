502f09be8f4837756ff283d38061de95188b254b
Felipe Mendonca <felipemom2001@gmail.com>
2022-09-12 16:21:03 +0200

    feat: complete prueba 2

--- 2.js
+++ 2.js
@@ -3,10 +3,39 @@
 // crea una función numbersTop para obtener los tres elementos más repetidos ordenados de forma descendente por número de repeticiones.
 
 function numbersTop(array) {
+  if (!Array.isArray(array)) return null;
+
+  const itemsCount = {};
+
+  array.forEach((e) => {
+    if (itemsCount[e]) {
+      itemsCount[e]++;
+    } else {
+      itemsCount[e] = 1;
+    }
+  });
+
+  const sortedItemsCount = Object.values(itemsCount).sort((a, b) => b - a);
+  const mostRepeatedItens = [];
+
+  sortedItemsCount.forEach((e, i) => {
+    for (key in itemsCount) {
+      if (itemsCount[key] === e) {
+        // Asi como en la prueba anterior, opté por utilizar find en el array
+        // original para mantener los tipos de datos originales
+
+        mostRepeatedItens[i] = array.find((el) => el == key);
+      }
+    }
+  });
+
+  return mostRepeatedItens.slice(0, 3);
 }
 
 /**
  * TEST Ejercicio 2
  */
-numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1]) // [ 1, 3, 2 ]
-numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, 'a', 3]) // [ 'a', 3, 2 ]
+numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1]); // [ 1, 3, 2 ]
+numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, 'a', 3]); // [ 'a', 3, 2 ]
+
+module.exports.numbersTop = numbersTop;
---
+++ tests/2.test.js
@@ -0,0 +1,34 @@
+const { numbersTop } = require('../2');
+
+describe('Prueba 1', () => {
+  it('Should return null if the param provided is not an array', () => {
+    const notAnArray = {};
+
+    expect(numbersTop(notAnArray)).toBeNull();
+  });
+
+  it('Should return the 3 most repeated elements in descending order', () => {
+    const array1 = [3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1];
+    const array2 = [
+      'a',
+      3,
+      2,
+      'a',
+      2,
+      3,
+      'a',
+      3,
+      4,
+      'a',
+      'a',
+      1,
+      'a',
+      2,
+      'a',
+      3,
+    ];
+
+    expect(numbersTop(array1)).toEqual([1, 3, 2]);
+    expect(numbersTop(array2)).toEqual(['a', 3, 2]);
+  });
+});
