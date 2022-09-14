c9a679c2f957adac7b4cd89a5365536c5a05e964
Felipe Mendonca <felipemom2001@gmail.com>
2022-09-14 19:38:31 +0200

    docs: last typo fix before finish the test

--- ejercicios/1.js
+++ ejercicios/1.js
@@ -18,27 +18,27 @@ function findUniq(array) {
 
 //comentario 1:
 /**
- * Técnicamente esta verificación hecha en la primer linea no es necesaria para que la función funcione,
- * sin embargo opté por realizarla para poder aprovechar los recursos de vscode
+ * Técnicamente esta verificación hecha en la primera línea de la función no es necesaria para que ella funcione,
+ * sin embargo, opté por realizarla para poder aprovechar los recursos de VS Code
  * como los snippets y así mejorar la productividad, evitar errores y hacer la
  * función más robusta.
- * Podria hacer otras verificaciones, como si el array viene vazio por ejemplo,
- * pero juzgué innecesario hacerlo por el nombre de la funcion. Por su nombre, no tiene ningun
- * sentido llamarla con un array vazio. Haciendo eso estamos de acuerdo com el principio 'KISS',
- * al mantener las soluciones simples y con el YAGNI al no hacer algo que no se necesite aun,
- * resultando en un codigo mas limpio y efectivo.
+ * Podría hacer otras verificaciones, como si el array viene vacío por ejemplo,
+ * pero juzgué innecesario hacerlo por el nombre de la función. Por su nombre, no tiene ningún
+ * sentido llamarla con un array vacío. Haciendo eso estamos de acuerdo con el principio 'KISS',
+ * al mantener las soluciones simples y con el 'YAGNI' al no hacer algo que no se necesite aún,
+ * resultando en un código más limpio y efectivo.
  **/
 
 //comentario 2:
 /**
- * La funcion podria acabar con un 'return key' si su tipo esperado de return siempre fuera string.
- * El uso del metodo find arriba es para garantizar que devuelve el elemento original del array, de modo que el tipo
- * de dato del elemento no sea alterado de 1 para '1' por ejemplo y con eso podemos cumplir exactamente con
+ * La función podría acabar con un 'return key' si su tipo esperado de return siempre fuera string.
+ * El uso del método find arriba es para garantizar que devuelve el elemento original del array, de modo que el tipo
+ * de dato del elemento no sea alterado de 1 para '1' por ejemplo, y con eso podemos cumplir exactamente con
  * el expect en los dos casos.
- * Al no hacerlo, podrias pasar de parametro un array de numeros y recibir de return una string en
- * lugar de un numero, lo que, dependendo del caso, no estaria perfectamente correcto y obligaria al cliente a hacer validaciones.
- * De esta forma, la funcion es mas confiable y previsible porque siempre retorna exactamente segun lo que tu le pasas de parametro.
- * Si passas un array de numeros, muy problamente esperas recibir un numero.
+ * Al no hacerlo, podrías pasar de parámetro un array de números y recibir de return una string en
+ * lugar de un número, lo que, dependiendo del caso, no estaría perfectamente correcto y obligaría al cliente a hacer validaciones.
+ * De esta forma, la función es más confiable y previsible porque siempre retorna exactamente según lo que tú le pasas de parámetro.
+ * Si pasas un array de números, muy probablemente esperas recibir un número como respuesta y no una string.
  **/
 
 /**
